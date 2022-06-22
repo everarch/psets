0877a8eae469c3c18b458d0a859f3d3114713fc4
Brandon Hill <107976177+everarch@users.noreply.github.com>
2022-06-22 01:05:15 -0500

    Add Regex

---
+++ hackerrank/regex/intro/specific-string.py
@@ -0,0 +1,17 @@
+/**
+ *
+ * Matching Specific String
+ *
+ * https://www.hackerrank.com/challenges/matching-specific-string/problem
+ *
+ **/
+
+Regex_Pattern = r'hackerrank'	# Do not delete 'r'.
+
+import re
+
+Test_String = input()
+
+match = re.findall(Regex_Pattern, Test_String)
+
+print("Number of matches :", len(match))
