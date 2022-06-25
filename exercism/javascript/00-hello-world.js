9374bf7302bad8710fb515e95d96ecb6d60b292a
Brandon Hill <107976177+everarch@users.noreply.github.com>
2022-06-25 18:32:52 -0500

    Add exercism

---
+++ exercism/javascript/00-hello-world.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * Hello, World!
+ *
+ * https://exercism.org/tracks/javascript/exercises/hello-world
+ *
+ **/
+
+export function hello() {
+  return 'Hello, World!';
+}
+
+/**
+ *
+ * 
+
+TEST CODE
+
+import { hello } from './hello-world';
+describe('Hello World', () => {
+  test('Say Hi!', () => {
+    expect(hello()).toEqual('Hello, World!');
+  });
+});
+
+
+*
+*
+**/
