7589e0d044f7a0b9f99b349616f5c64e42063b3d
Brandon Hill <107976177+everarch@users.noreply.github.com>
2022-07-03 17:05:38 -0500

    Add new problems

---
+++ hackerrank/regex/repetitons/03-one-repetitions.py
@@ -0,0 +1,11 @@
+#
+# Matching One Or More Repetitions
+#
+# https://www.hackerrank.com/challenges/matching-one-or-more-repititions/problem
+#
+
+Regex_Pattern = r'^\d+[A-Z]+[a-z]+$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
---
+++ hackerrank/regex/repetitons/04-end-boundaries.py
@@ -0,0 +1,11 @@
+#
+# Matching Ending Items
+#
+# https://www.hackerrank.com/challenges/matching-ending-items/problem
+#
+
+Regex_Pattern = r'^[a-zA-Z]*s$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
