64f7220f50e634d557e7c543feea20d2b85f1706
Brandon Hill <107976177+everarch@users.noreply.github.com>
2022-07-02 22:08:03 -0500

    Add new problems

---
+++ hackerrank/regex/repetitons/02-zero-repetitions.py
@@ -0,0 +1,11 @@
+#
+# Matching Zero Or More Repetitions
+#
+# https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem
+#
+
+Regex_Pattern = r'^\d{2,}[a-z]*[A-Z]*$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
