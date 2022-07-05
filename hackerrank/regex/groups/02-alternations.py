c8c019c1f1243afb9d77746468e2e8acc511caa8
Brandon Hill <107976177+everarch@users.noreply.github.com>
2022-07-05 18:09:41 -0500

    Add new problems

---
+++ hackerrank/regex/groups/02-alternations.py
@@ -0,0 +1,11 @@
+#
+# Alternative Matching
+#
+# https://www.hackerrank.com/challenges/alternative-matching/problem
+#
+
+Regex_Pattern = r'^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
